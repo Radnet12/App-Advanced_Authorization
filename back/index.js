488fb1863c9674c16c4a43e564b9089af8eb8ca4
Radnet12 <sergeyvasylenko4@gmail.com>
2022-01-02 13:05:38 +0200

    added models

--- back/index.js
+++ back/index.js
@@ -4,14 +4,24 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
 
 // Variables
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-const startServer = () => {
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+const startServer = async () => {
     try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         app.listen(PORT, () =>
             console.log(`Server started on the PORT = ${PORT}`)
         );
